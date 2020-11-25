d0f680426f2340e1c1541d07b42df442582c8cd0
Edin Jašarević <edinjasar14@gmail.com>
2020-11-25 14:00:12 +0100

    installing analyzer as extensions

--- LoanManager.Core/Domain/Rules/LoanSizeLoanApprovalRule.cs
+++ LoanManager.Core/Domain/Rules/LoanSizeLoanApprovalRule.cs
@@ -13,20 +13,17 @@ namespace LoanManager.Core.Domain
         {
             double loanAmount = application.LoanAmount;
 
-            switch (loanAmount)
-            {
-                case double n when (n <= 10_000):
-                    // We don't issue loans less than $50,000
-                    return false;
-                case double n when (n > 10_000 && n < 1_000_000):
-                    // Loans from $50,000 to $1,000,000 are OK
-                    return true;
-                case double n when (n > 1_000_000):
-                    // Do not issue loans over $1,000,000
-                    return false;
-                default:
-                    return false;
-            }
+            if (loanAmount is double n1 && (n1 <= 10_000))
+            // We don't issue loans less than $50,000
+                return false;
+            else if (loanAmount is double n2 && (n2 > 10_000 && n2 < 1_000_000))
+            // Loans from $50,000 to $1,000,000 are OK
+                return true;
+            else if (loanAmount is double n3 && (n3 > 1_000_000))
+            // Do not issue loans over $1,000,000
+                return false;
+            else
+                return false;
         }
 
 
